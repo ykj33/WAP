cbe8dbc74dcd2bd5749107cfd6b5c8f34f77c5ab
ykj33 <dnsrjswnd@gmail.com>
2021-08-04 21:33:35 +0900

    20210804 로그인 시 아이디가 틀릴 경우 경고메시지 출력 및 페이지 초기화

--- php/login.html.php
+++ php/login.html.php
@@ -8,20 +8,28 @@ try {
     $stmh = $pdo->prepare($sql);
     $stmh->bindValue(':user_id', $user_id, PDO::PARAM_STR);
     $stmh->execute();
-    while ($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
-        if ($password == $row['password']) {
-            session_start();
-            $_SESSION['user_id'] = $user_id;
-            $_SESSION['password'] = $password;
-            $_SESSION['team_name'] = $row['team_name'];
-            Header("Location:../index.html");
-        } else {
+    $count = $stmh->rowCount();
+    print "카운트".$count;
+    if ($count == 0) {
+        print $count;
+        echo '<script>alert("잘못된 아이디를 입력하셨습니다. 다시 입력해주세요.");location.href="../login.html";</script>';
+    } else {
+        while ($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
+            if ($user_id == $row['user_id'] && $password == $row['password']) {
+                session_start();
+                $_SESSION['user_id'] = $user_id;
+                $_SESSION['password'] = $password;
+                $_SESSION['team_name'] = $row['team_name'];
+                Header("Location:../index.html");
+            } else {
 
-            echo '<script>alert("잘못된 계정정보를 입력하셨습니다. 다시 입력해주세요.");location.href="../login.html";</script>';
+                echo '<script>alert("잘못된 계정정보를 입력하셨습니다. 다시 입력해주세요.");location.href="../login.html";</script>';
 
 
 //            Header("Location:./login.html.php");
+            }
         }
+
     }
 } catch (Exception $exception) {
     die('오류:' . $exception->getMessage());
